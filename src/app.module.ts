121e9eb654aa829626be2584dc8742809632c0f8
leehyebin <970905hyebin@gmail.com>
2024-05-03 12:39:22 +0900

    feat:rds 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  ENV_DB_DATABASE_KEY,
+  ENV_DB_HOST_KEY,
+  ENV_DB_PASSWORD_KEY,
+  ENV_DB_PORT_KEY,
+  ENV_DB_USERNAME_KEY,
+} from './common/const';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>(ENV_DB_HOST_KEY),
+        port: configService.get<number>(ENV_DB_PORT_KEY),
+        username: configService.get<string>(ENV_DB_USERNAME_KEY),
+        password: configService.get<string>(ENV_DB_PASSWORD_KEY),
+        database: configService.get<string>(ENV_DB_DATABASE_KEY),
+        autoLoadEntities: true,
+        synchronize: true,
+        //TODO 삼항연산자 dev에서만 되게
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
