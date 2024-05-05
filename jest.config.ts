21f8657ad0b8bb210efba7a9a674cbd9349a59cb
leehyebin <970905hyebin@gmail.com>
2024-05-05 19:42:56 +0900

    fix: jest 설정 변경 jeset.config.ts, jest.setup.ts 분리

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { InitialOptions } from '@jest/types/build/Config';
+
+const config: InitialOptions = {
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  setupFilesAfterEnv: ['../jest.setup.ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
+
+export default config;
