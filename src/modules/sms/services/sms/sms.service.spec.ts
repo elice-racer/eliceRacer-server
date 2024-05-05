7ba70528dd1f2c997a370187d7aaa296073d9a05
leehyebin <970905hyebin@gmail.com>
2024-05-05 20:28:13 +0900

    test: sms 유닛테스트 추가 인증문자 성공 케이스에 대한 테스트 추가

---
+++ src/modules/sms/services/sms/sms.service.spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { SmsService } from './sms.service';
+import { ConfigService } from '@nestjs/config';
+
+describe('SmsService', () => {
+  let service: SmsService;
+  // let configService: ConfigService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [SmsService, ConfigService],
+    }).compile();
+
+    service = module.get<SmsService>(SmsService);
+    // configService = module.get<ConfigService>(ConfigService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('sendVerificationCode', () => {
+    it('인증 문자 전송에 성공한다', async () => {
+      const mockSendOne = jest.fn().mockResolvedValue({ statusCode: '2000' });
+      service['messageService'] = { sendOne: mockSendOne };
+
+      const result = await service.sendVerificationCode(
+        '01012345678',
+        '123456',
+      );
+
+      expect(result).toBe('Success');
+    });
+  });
+});
