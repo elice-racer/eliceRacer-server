a639d32c4039c9e44fdec7852670e6c4627aa56c
leehyebin <970905hyebin@gmail.com>
2024-05-07 01:47:54 +0900

    feat: 인증 문자 발송 기능 추가

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
 import { SmsModule } from '../sms/sms.module';
 import { UserModule } from '../user/user.module';
+import { smsVerificationRepository } from './repositories';
 
 @Module({
   imports: [SmsModule, UserModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, smsVerificationRepository],
 })
 export class AuthModule {}
