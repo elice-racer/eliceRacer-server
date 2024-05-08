3a18b81010c83ddc24beaf6fe16fe23ff51f227f
leehyebin <970905hyebin@gmail.com>
2024-05-09 03:03:23 +0900

    feat: autn controller 로직 추가 번호 인증, 인증번호 검증, 로그인, 토큰 재발급, 로그아웃 로직 추가

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,7 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
+import { VerifyCodeReqDto } from '../dto/verify-code-req.dto';
+import {
+  LoginResDto,
+  LogoutReqDto,
+  RefreshReqDto,
+  RefreshResDto,
+  VerifyCodeResDto,
+} from '../dto';
+import { LoginReqDto } from '../dto/login-req.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/send-verification')
+  async sendVerificationCode(@Body('phoneNumber') phoneNumber: string) {
+    return await this.authService.handlePhoneVerification(phoneNumber);
+  }
+
+  @Post('/verify-code')
+  async verifyAuthCode(
+    @Body() verifyCodeReqDto: VerifyCodeReqDto,
+  ): Promise<VerifyCodeResDto> {
+    return await this.authService.handleCodeVerification(
+      verifyCodeReqDto.phoneNumber,
+      verifyCodeReqDto.authCode,
+    );
+  }
+
+  @Post('/login')
+  async login(@Body() loginReqDto: LoginReqDto): Promise<LoginResDto> {
+    return await this.authService.login(
+      loginReqDto.identifier,
+      loginReqDto.password,
+    );
+  }
+
+  @Post('/refresh')
+  async refresh(@Body() refreshReqDto: RefreshReqDto): Promise<RefreshResDto> {
+    return await this.authService.refresh(refreshReqDto.refreshToken);
+  }
+
+  @Post('/logout')
+  async logout(@Body() logoutReqDto: LogoutReqDto) {
+    return await this.authService.logout(logoutReqDto.refreshToken);
+  }
 }
