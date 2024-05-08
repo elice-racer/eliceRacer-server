6d47ab0a01b57e85bda3ba73a839db3cedb0e760
leehyebin <970905hyebin@gmail.com>
2024-05-09 02:57:32 +0900

    test: auth controller 테스트 코드 추가 전화번호 인증, 인증번호 검증, 로그인, 토큰 재발급, 로그아웃 테스트 코드 추가

--- src/modules/auth/controllers/auth.controller.spec.ts
+++ src/modules/auth/controllers/auth.controller.spec.ts
@@ -3,20 +3,114 @@ import { AuthController } from './auth.controller';
 import { AuthService } from '../services/auth.service';
 import { UserService } from 'src/modules/user/services/user.service';
 import { SmsService } from 'src/modules/sms/services/sms.service';
+import { SmsVerificationRepository } from '../repositories';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { RefreshTokenRepository } from '../repositories/refresh-token.repository';
+import {
+  LoginReqDto,
+  LoginResDto,
+  LogoutReqDto,
+  RefreshReqDto,
+  RefreshResDto,
+  VerifyCodeReqDto,
+  VerifyCodeResDto,
+} from '../dto';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: jest.Mocked<AuthService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService, SmsService, UserService],
+      providers: [
+        AuthService,
+        SmsService,
+        UserService,
+        SmsVerificationRepository,
+        JwtService,
+        ConfigService,
+        RefreshTokenRepository,
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('sendVerificationCode', () => {
+    it('전화번호로 인증 코드를 발송한다.', async () => {
+      authService.handlePhoneVerification.mockResolvedValue('Success');
+      const result = await controller.sendVerificationCode('01012345678');
+      expect(result).toBe('Success');
+    });
+  });
+
+  describe('verifyCode', () => {
+    it('인증 번호가 맞는지 검증한다', async () => {
+      const reqDto: VerifyCodeReqDto = {
+        phoneNumber: '01012345678',
+        authCode: '123456',
+      };
+      const resDto: VerifyCodeResDto = {
+        email: 'user@example.com',
+        realName: '홍길동',
+        track: [],
+      };
+
+      authService.handleCodeVerification.mockResolvedValue(resDto);
+
+      const result = await controller.verifyAuthCode(reqDto);
+
+      expect(authService.handleCodeVerification).toHaveBeenCalledWith(
+        reqDto.phoneNumber,
+        reqDto.authCode,
+      );
+      expect(result).toEqual(resDto);
+    });
+  });
+
+  describe('login', () => {
+    it('올바른 아이디 또는 이메일, 비밀번호를 이용해서 로그인에 성공한다', async () => {
+      const reqDto: LoginReqDto = {
+        identifier: 'user@example.com',
+        password: 'password',
+      };
+      const resDto: LoginResDto = {
+        accessToken: 'accessToken',
+        refreshToken: 'refreshToken',
+      };
+      authService.login.mockResolvedValue(resDto);
+      const result = await controller.login(reqDto);
+
+      expect(result).toBe(resDto);
+    });
+  });
+
+  describe('refresh', () => {
+    it('리프레시 토큰을 이용해 액세스 토큰을 발급한다', async () => {
+      const reqDto: RefreshReqDto = {
+        refreshToken: 'refreshToken',
+      };
+      const resDto: RefreshResDto = {
+        accessToken: 'accessToken',
+      };
+
+      authService.refresh.mockResolvedValue(resDto);
+      const result = await controller.refresh(reqDto);
+      expect(result).toBe(resDto);
+    });
+  });
+
+  describe('logout', () => {
+    it('로그아웃한다', async () => {
+      const resDto: LogoutReqDto = { refreshToken: 'refreshToken' };
+      await controller.logout(resDto);
+    });
   });
 });
