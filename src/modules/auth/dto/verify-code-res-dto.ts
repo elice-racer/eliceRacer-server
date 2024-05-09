67ae3734408c3b5fd05f38b564a081200bb4bbb2
leehyebin <970905hyebin@gmail.com>
2024-05-09 20:00:18 +0900

    feat: 어드미 회원가입 기능 추가

--- src/modules/auth/dto/verify-code-res-dto.ts
+++ src/modules/auth/dto/verify-code-res-dto.ts
@@ -3,5 +3,5 @@ import { TrackDto } from 'src/modules/track/dto';
 export type VerifyCodeResDto = {
   email: string | null;
   realName: string | null;
-  track: TrackDto[];
+  tracks: TrackDto[];
 };
