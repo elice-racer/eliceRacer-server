6c6169efffdee93f027532db81ff9bbee2c4943b
leehyebin <970905hyebin@gmail.com>
2024-05-09 20:13:08 +0900

    feat: 이메일 전송 기능 추가

---
+++ src/modules/admin/repositories/verify-email.repository.ts
@@ -0,0 +1,23 @@
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+
+@Injectable()
+export class VerifyEmailRepository {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  async getRefreshToken(key: string) {
+    return this.cacheManager.get(key);
+  }
+  async setRefreshToken(
+    key: string,
+    value: string,
+    ttl: number,
+  ): Promise<void> {
+    return this.cacheManager.set(key, value, ttl);
+  }
+
+  async deleteRefreshToken(key: string): Promise<void> {
+    this.cacheManager.del(key);
+  }
+}
