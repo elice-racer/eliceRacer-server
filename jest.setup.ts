c95fd814edebf19892a63013a8282c13b530f5af
leehyebin <970905hyebin@gmail.com>
2024-05-09 19:55:33 +0900

    feat: logger 추가

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,6 +3,7 @@ import {
   ENV_SMS_API_SECRET_KEY,
   ENV_SMS_FROM_NUMBER_KEY,
 } from 'src/common/const';
+
 //라이브러리
 jest.mock('argon2');
 jest.mock('@nestjs/config');
@@ -11,15 +12,19 @@ jest.mock('@nestjs/jwt');
 //Servcie
 jest.mock('./src/modules/sms/services/sms.service');
 jest.mock('./src/modules/user/services/user.service');
+jest.mock('./src/modules/auth/services/auth.service');
+jest.mock('./src/modules/mail/mail.service');
 
 //Repository
 jest.mock('./src/modules/auth/repositories/sms-verification.repository');
 jest.mock('./src/modules/user/repositories/user.repository');
 jest.mock('./src/modules/auth/repositories/refresh-token.repository');
+jest.mock('./src/modules/admin/repositories/verify-email.repository');
 
 //utils
 jest.mock('./src/common/utils/verification-number-generator');
 jest.mock('./src/common/utils/password-hash');
+jest.mock('./src/common/utils/verification-token-genertator');
 
 // configServiceSetup;
 jest.mock('@nestjs/config', () => ({
@@ -27,11 +32,11 @@ jest.mock('@nestjs/config', () => ({
     get: jest.fn((key) => {
       switch (key) {
         case ENV_SMS_API_KEY:
-          return 'mock_api_key'; // 가짜 API 키
+          return 'mock_api_key';
         case ENV_SMS_API_SECRET_KEY:
-          return 'mock_secret_key'; // 가짜 비밀 키
+          return 'mock_secret_key';
         case ENV_SMS_FROM_NUMBER_KEY:
-          return 'mock_phone_number'; // 가짜 전화번호
+          return 'mock_phone_number';
         default:
           return null;
       }
