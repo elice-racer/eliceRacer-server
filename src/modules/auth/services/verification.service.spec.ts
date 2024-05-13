b610c7805ab8b2ddd52ddbae1aad6fa65d0a9dff
leehyebin <970905hyebin@gmail.com>
2024-05-13 16:03:19 +0900

    fix: verify Code return 값 변경 기존: veifyCode 내부에서 에러처리, 변경: boolean값 반환 외부에서 에러처리

--- src/modules/auth/services/verification.service.spec.ts
+++ src/modules/auth/services/verification.service.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VerificationService } from './verification.service';
-import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { VerificationRepository } from '../repositories';
 
 jest.unmock('./verification.service');
@@ -21,33 +20,36 @@ describe('VerificationService', () => {
   });
 
   describe('verifyCode', () => {
-    it('유효시간이 지난 인증번호를 입력하면 NotFoundException을 반환한다', async () => {
+    it('유효시간이 지난 인증번호를 입력하면 false을 반환한다', async () => {
+      const phoneNumber = '01012345678';
+      const inputCode = '123450';
       jest.spyOn(service, 'getVerificationCode').mockResolvedValue(null);
 
-      await expect(service.verifyCode('01012345678', '123456')).rejects.toThrow(
-        NotFoundException,
-      );
+      const result = await service.verifyCode(phoneNumber, inputCode);
+
+      expect(result).toBe(false);
     });
 
-    it('유효하지 않은 인증번호를 입력하면 BadRequestException를 반환한다', async () => {
+    it('유효하지 않은 인증번호를 입력하면 false를 반환한다', async () => {
+      const phoneNumber = '01012345678';
       const inputCode = '123450';
       const storedCode = '123456';
 
       jest.spyOn(service, 'getVerificationCode').mockResolvedValue(storedCode);
 
-      await expect(
-        service.verifyCode('01012345678', inputCode),
-      ).rejects.toThrow(BadRequestException);
+      const result = await service.verifyCode(phoneNumber, inputCode);
+
+      expect(result).toBe(false);
     });
 
-    it('유효시간 내에 올바른 인증번호를 입력하면 OK를 반환한다 ', async () => {
+    it('유효시간 내에 올바른 인증번호를 입력하면 true를 반환한다 ', async () => {
       const phoneNumber = '01012345678';
       const inputCode = '123456';
 
       jest.spyOn(service, 'getVerificationCode').mockResolvedValue(inputCode);
       const result = await service.verifyCode(phoneNumber, inputCode);
 
-      expect(result).toBe('OK');
+      expect(result).toBe(true);
     });
   });
 });
