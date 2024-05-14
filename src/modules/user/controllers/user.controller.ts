964d6e5396617f29cdf5ad2012cc85687fca2663
leehyebin <970905hyebin@gmail.com>
2024-05-15 04:33:25 +0900

    feat: user update 추가

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from '../services/user.service';
-import { CreateUserDto } from '../dto';
+import { CreateUserDto, updateReqDto } from '../dto';
+import { CurrentUser } from 'src/common/decorators';
+import { User } from '../entities';
+import { AdminGuard, JwtAuthGuard } from 'src/common/guards';
 
 @Controller('users')
 export class UserController {
@@ -9,5 +19,21 @@ export class UserController {
   @Post('/signup')
   async signup(@Body() createUserDto: CreateUserDto) {
     await this.userService.handleSignUp(createUserDto);
+  }
+
+  @Post('/:id')
+  @UseGuards(JwtAuthGuard)
+  async updateMypage(@Body() dto: updateReqDto, @CurrentUser() user: User) {
+    console.log(dto, user);
+  }
+
+  @Patch('/tracks/:id')
+  @UseGuards(AdminGuard)
+  async updateUserTrack(
+    @Param('id') id: string,
+    @Body('trackName') trackNames: string[],
+  ) {
+    const result = await this.userService.updateUserTracks(id, trackNames);
+    console.log(result);
   }
 }
