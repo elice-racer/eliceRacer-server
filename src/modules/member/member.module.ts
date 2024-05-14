63ab0b552bd7570a86575c6f4016f46c637457df
leehyebin <970905hyebin@gmail.com>
2024-05-14 15:56:22 +0900

    feat: member 등록 로직 추가

--- src/modules/member/member.module.ts
+++ src/modules/member/member.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MemberService } from './services/member.service';
 import { UserModule } from '../user/user.module';
 import { MemberController } from './controllers/member.controller';
+import { TrackModule } from '../track/track.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TrackModule],
   controllers: [MemberController],
   providers: [MemberService],
 })
