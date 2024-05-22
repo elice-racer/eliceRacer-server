084c2f1ed2892f2fb332a5f17cc5d84d58716627
leehyebin <970905hyebin@gmail.com>
2024-05-22 16:22:56 +0900

    test: track update 테스트 추가

--- src/modules/track/services/track.service.spec.ts
+++ src/modules/track/services/track.service.spec.ts
@@ -3,6 +3,7 @@ import { TrackService } from './track.service';
 import { TrackDto, TrackResDto } from '../dto';
 import { TrackRespository } from '../repositories/track.repository';
 import { Track } from '../entities';
+import { BusinessException } from 'src/exception';
 
 describe('TrackService', () => {
   let service: TrackService;
@@ -12,6 +13,7 @@ describe('TrackService', () => {
   };
 
   const resDto: TrackResDto = {
+    id: 'uuid',
     trackName: 'Track1',
   };
 
@@ -31,6 +33,7 @@ describe('TrackService', () => {
   describe('createTrack', () => {
     it('track을 생성한다', async () => {
       const track = new Track();
+      track.id = 'uuid';
       track.trackName = resDto.trackName;
 
       trackRepo.createTrack.mockResolvedValue(track);
@@ -41,11 +44,20 @@ describe('TrackService', () => {
   });
 
   describe('updateTrack', () => {
+    it('track이 존재하지 않으면 businessException을 반환한다', async () => {
+      trackRepo.findOneBy.mockResolvedValue(undefined);
+
+      await expect(service.updateTrack('uuid', dto)).rejects.toThrow(
+        BusinessException,
+      );
+    });
     it('track을 업데이트 한다', async () => {
       const track = new Track();
-      track.trackName = resDto.trackName;
-      trackRepo.updateTrack.mockResolvedValue(track);
+      track.id = 'uuid';
+      track.trackName = dto.trackName;
 
+      trackRepo.findOneBy.mockResolvedValue(track);
+      trackRepo.save.mockResolvedValue(track);
       const result = await service.updateTrack('trackId', dto);
 
       expect(result).toEqual(resDto);
