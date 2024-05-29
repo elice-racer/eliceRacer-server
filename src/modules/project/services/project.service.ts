5755ccdc34a8e8acbdf4af47ea5077787c148e45
leehyebin <970905hyebin@gmail.com>
2024-05-29 19:29:30 +0900

    feat: 프로젝트 가져오기 로직 추가

--- src/modules/project/services/project.service.ts
+++ src/modules/project/services/project.service.ts
@@ -8,11 +8,24 @@ import { BusinessException } from 'src/exception';
 @Injectable()
 export class ProjectService {
   constructor(
-    private readonly ProejctRepo: ProjectRepository,
+    private readonly projectRepo: ProjectRepository,
     private readonly tarckRepo: TrackRepository,
   ) {}
 
-  async getProject() {}
+  async getProject(projectId: string) {
+    const project = this.projectRepo.findOneBy({ id: projectId });
+
+    if (!project)
+      throw new BusinessException(
+        `project`,
+        `해당 프로젝트가 존재하지 않습니다.`,
+        `해당 프로젝트가 존재하지 않습니다.`,
+        HttpStatus.NOT_FOUND,
+      );
+
+    return project;
+  }
+
   async getAllProejcts() {}
   async getProjectByTrack() {}
 
@@ -20,7 +33,7 @@ export class ProjectService {
     projectId: string,
     dto: UpdateProjectReqDto,
   ): Promise<Project> {
-    const project = await this.ProejctRepo.findOneBy({ id: projectId });
+    const project = await this.projectRepo.findOneBy({ id: projectId });
     if (!project)
       throw new BusinessException(
         `project`,
@@ -33,7 +46,8 @@ export class ProjectService {
     project.round = dto.round;
     project.startDate = new Date(dto.startDate);
     project.endDate = new Date(dto.endDate);
-    return project;
+
+    return this.projectRepo.save(project);
   }
 
   async deleteProject() {}
