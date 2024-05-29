895fcb9bd7932b13dd80d491d86a90ec5dd1a6c4
leehyebin <970905hyebin@gmail.com>
2024-05-29 19:28:31 +0900

    test: 프로젝트 가져오기 테스트 추가

--- src/modules/project/services/project.service.spec.ts
+++ src/modules/project/services/project.service.spec.ts
@@ -23,6 +23,28 @@ describe('ProjectService', () => {
     expect(service).toBeDefined();
   });
 
+  describe('getProject', () => {
+    const projectId = 'uuid';
+
+    it('프로젝트가 존재하지 않으면 BusinessException을 던진다', async () => {
+      projectRepo.findOneBy.mockReturnValue(undefined);
+
+      await expect(service.getProject(projectId)).rejects.toThrow(
+        BusinessException,
+      );
+    });
+
+    it('프로젝트가 존재하면 해당 프로젝트를 반환해준다', async () => {
+      const project = new Project();
+
+      projectRepo.findOneBy.mockResolvedValue(project);
+
+      const result = await service.getProject(projectId);
+
+      expect(result).toEqual(project);
+    });
+  });
+
   describe('updateProject', () => {
     const projectId = 'uuid';
     const dto: UpdateProjectReqDto = {
@@ -31,7 +53,7 @@ describe('ProjectService', () => {
       startDate: '2024-01-01',
       endDate: '2024-02-02',
     };
-    it('update할 projec가 존재하지 않으면 BusinessException을 던진다', async () => {
+    it('수정할 프로젝트가 존재하지 않으면 BusinessException을 던진다', async () => {
       projectRepo.findOneBy.mockReturnValue(undefined);
 
       await expect(service.updateProject(projectId, dto)).rejects.toThrow(
@@ -39,11 +61,12 @@ describe('ProjectService', () => {
       );
     });
 
-    it('성공적으로 project를 update하고 반환한다', async () => {
+    it('성공적으로 프로젝트를 수정하고 반환한다', async () => {
       const project = new Project();
       project.projectName = dto.projectName;
       project.round = dto.round;
-
+      project.startDate = new Date(dto.startDate);
+      project.endDate = new Date(dto.endDate);
       projectRepo.findOneBy.mockResolvedValue(project);
 
       const result = await service.updateProject(projectId, dto);
