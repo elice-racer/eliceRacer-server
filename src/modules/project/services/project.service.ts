f6c873acd79355a6be7ae012de794761ac64a570
leehyebin <970905hyebin@gmail.com>
2024-05-29 16:58:52 +0900

    feat: 프로젝트 업데이트 추가

--- src/modules/project/services/project.service.ts
+++ src/modules/project/services/project.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { ProjectRepository } from '../repositories/project.repository';
 import { TrackRepository } from 'src/modules/track/repositories';
+import { Project } from '../entities';
+import { UpdateProjectReqDto } from '../dto';
+import { BusinessException } from 'src/exception';
 
 @Injectable()
 export class ProjectService {
@@ -8,4 +11,30 @@ export class ProjectService {
     private readonly ProejctRepo: ProjectRepository,
     private readonly tarckRepo: TrackRepository,
   ) {}
+
+  async getProject() {}
+  async getAllProejcts() {}
+  async getProjectByTrack() {}
+
+  async updateProject(
+    projectId: string,
+    dto: UpdateProjectReqDto,
+  ): Promise<Project> {
+    const project = await this.ProejctRepo.findOneBy({ id: projectId });
+    if (!project)
+      throw new BusinessException(
+        `project`,
+        `해당 프로젝트가 존재하지 않습니다.`,
+        `해당 프로젝트가 존재하지 않습니다.`,
+        HttpStatus.NOT_FOUND,
+      );
+
+    project.projectName = dto.projectName;
+    project.round = dto.round;
+    project.startDate = new Date(dto.startDate);
+    project.endDate = new Date(dto.endDate);
+    return project;
+  }
+
+  async deleteProject() {}
 }
