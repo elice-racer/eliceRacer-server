9f49d7d3ea6c38b7803a4955974f96ba687cf0b2
leehyebin <970905hyebin@gmail.com>
2024-06-13 21:53:17 +0900

    feat: 마이그레이션 스크립트 추가

---
+++ src/migration/1718281241670-SyncWithRDS.ts
@@ -0,0 +1,75 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class SyncWithRDS1687548392843 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.startTransaction();
+    try {
+      // messages 테이블 변경
+      await queryRunner.query(
+        `ALTER TABLE "messages" ALTER COLUMN "content" TYPE character varying(500)`,
+      );
+
+      // notices 테이블 변경
+      await queryRunner.query(
+        `ALTER TABLE "notices" ALTER COLUMN "content" TYPE character varying(1000)`,
+      );
+
+      // users 테이블 변경
+      await queryRunner.query(
+        `ALTER TABLE "users" ADD "profileImage" character varying`,
+      );
+      await queryRunner.query(
+        `ALTER TABLE "users" ALTER COLUMN "phone_number" TYPE character varying(11)`,
+      );
+      await queryRunner.query(
+        `ALTER TABLE "users" ALTER COLUMN "comment" TYPE character varying(100)`,
+      );
+      await queryRunner.query(
+        `ALTER TABLE "users" ALTER COLUMN "description" TYPE character varying(1000)`,
+      );
+      await queryRunner.query(
+        `ALTER TABLE "users" ALTER COLUMN "tmi" TYPE character varying(1000)`,
+      );
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.startTransaction();
+    try {
+      // users 테이블 롤백
+      await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "profileImage"`);
+      await queryRunner.query(
+        `ALTER TABLE "users" ALTER COLUMN "phone_number" TYPE character varying`,
+      );
+      await queryRunner.query(
+        `ALTER TABLE "users" ALTER COLUMN "comment" TYPE character varying`,
+      );
+      await queryRunner.query(
+        `ALTER TABLE "users" ALTER COLUMN "description" TYPE character varying`,
+      );
+      await queryRunner.query(
+        `ALTER TABLE "users" ALTER COLUMN "tmi" TYPE character varying`,
+      );
+
+      // notices 테이블 롤백
+      await queryRunner.query(
+        `ALTER TABLE "notices" ALTER COLUMN "content" TYPE character varying`,
+      );
+
+      // messages 테이블 롤백
+      await queryRunner.query(
+        `ALTER TABLE "messages" ALTER COLUMN "content" TYPE character varying`,
+      );
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    }
+  }
+}
