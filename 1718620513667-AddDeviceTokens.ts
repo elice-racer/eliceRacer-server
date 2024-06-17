2c6704c70f2057717f67f3ce95877d954a0b146c
leehyebin <970905hyebin@gmail.com>
2024-06-17 19:37:31 +0900

    feat: 마이그레이션 스크립트 추가

---
+++ 1718620513667-AddDeviceTokens.ts
@@ -0,0 +1,61 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddDeviceTokens1718620513667 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.startTransaction();
+    try {
+      // Modify 'type' column in 'chats' table to be NOT NULL
+      await queryRunner.query(`
+                ALTER TABLE chats ALTER COLUMN type SET NOT NULL;
+            `);
+
+      // Create 'device_token' table
+      await queryRunner.query(`
+                CREATE TABLE device_token (
+                    id uuid PRIMARY KEY NOT NULL,
+                    created_at timestamp without time zone NOT NULL,
+                    updated_at timestamp without time zone NOT NULL,
+                    token character varying NOT NULL,
+                    userId uuid
+                );
+            `);
+
+      // Modify 'users' table: move 'password' column
+      await queryRunner.query(`
+                ALTER TABLE users DROP COLUMN password;
+                ALTER TABLE users ADD COLUMN password character varying;
+            `);
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.startTransaction();
+    try {
+      // Revert 'type' column in 'chats' table to allow NULL
+      await queryRunner.query(`
+                ALTER TABLE chats ALTER COLUMN type DROP NOT NULL;
+            `);
+
+      // Drop 'device_token' table
+      await queryRunner.query(`
+                DROP TABLE device_token;
+            `);
+
+      // Revert 'users' table: move 'password' column back
+      await queryRunner.query(`
+                ALTER TABLE users DROP COLUMN password;
+                ALTER TABLE users ADD COLUMN password character varying;
+            `);
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    }
+  }
+}
