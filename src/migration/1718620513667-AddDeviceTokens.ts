ddf166f9398037be2a0a82e41758794473d3bf45
leehyebin <970905hyebin@gmail.com>
2024-06-18 01:50:07 +0900

    fix: 마이그레이션 스크립트 수정, user controller 수정

--- src/migration/1718620513667-AddDeviceTokens.ts
+++ src/migration/1718620513667-AddDeviceTokens.ts
@@ -2,48 +2,33 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddDeviceTokens1718620513667 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.startTransaction();
-    try {
-      // Modify 'type' column in 'chats' table to be NOT NULL
-      await queryRunner.query(`
-                ALTER TABLE chats ALTER COLUMN type SET NOT NULL;
-            `);
+    await queryRunner.query(`
+      ALTER TABLE chats ALTER COLUMN type SET NOT NULL;
+    `);
 
-      // Create 'device_token' table
-      await queryRunner.query(`
-                CREATE TABLE device_token (
-                    id uuid PRIMARY KEY NOT NULL,
-                    created_at timestamp without time zone NOT NULL,
-                    updated_at timestamp without time zone NOT NULL,
-                    token character varying NOT NULL,
-                    userId uuid
-                );
-            `);
+    await queryRunner.query(`
+      CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    `);
 
-      await queryRunner.commitTransaction();
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-      throw err;
-    }
+    await queryRunner.query(`
+      CREATE TABLE device_token (
+        id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+        created_at timestamp without time zone NOT NULL DEFAULT now(),
+        updated_at timestamp without time zone NOT NULL DEFAULT now(),
+        token character varying NOT NULL,
+        "userId" uuid,
+        CONSTRAINT fk_user FOREIGN KEY ("userId") REFERENCES users(id)
+      );
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.startTransaction();
-    try {
-      // Revert 'type' column in 'chats' table to allow NULL
-      await queryRunner.query(`
-                ALTER TABLE chats ALTER COLUMN type DROP NOT NULL;
-            `);
+    await queryRunner.query(`
+      ALTER TABLE chats ALTER COLUMN type DROP NOT NULL;
+    `);
 
-      // Drop 'device_token' table
-      await queryRunner.query(`
-                DROP TABLE device_token;
-            `);
-
-      await queryRunner.commitTransaction();
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-      throw err;
-    }
+    await queryRunner.query(`
+      DROP TABLE device_token;
+    `);
   }
 }
