c992f8612023c542501ab21b58b1be5ec9640458
leehyebin <970905hyebin@gmail.com>
2024-06-28 17:40:46 +0900

    refactor: team building 엑셀 업로드 수정

--- src/modules/admin/services/admin.service.ts
+++ src/modules/admin/services/admin.service.ts
@@ -1,14 +1,14 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { CreateAdminDto } from '../dto/create-admin.dto';
 import * as argon2 from 'argon2';
-import { convertDate, generateToken } from 'src/common/utils';
+import { excelDateToJSDate, generateToken } from 'src/common/utils';
 import { MailService } from 'src/modules/mail/mail.service';
 import { VerificationService } from 'src/modules/auth/services/verification.service';
 import { AdminRepository } from '../repositories';
 import { User, UserRole, UserStatus } from 'src/modules/user/entities';
 import { BusinessException } from 'src/exception';
 import { parseExcel } from 'src/common/utils';
-import { validateCoaches, validateData } from 'src/common/utils/data-validator';
+import { validateDataWithCoaches } from 'src/common/utils/data-validator';
 import { EntityManager, In } from 'typeorm';
 import { ProjectRepository } from 'src/modules/project/repositories/project.repository';
 import { TeamRepository } from 'src/modules/team/repositories/team.repository';
@@ -17,6 +17,7 @@ import { Team } from 'src/modules/team/entities/team.entity';
 import { Project } from 'src/modules/project/entities';
 import { TrackRepository } from 'src/modules/track/repositories';
 import { Officehour } from 'src/modules/officehour/entities/officehour.entity';
+import { Track } from 'src/modules/track/entities';
 
 @Injectable()
 export class AdminService {
@@ -83,9 +84,146 @@ export class AdminService {
   }
 
   //TODO 이동
+  // async createTeamAndProject(file: Express.Multer.File) {
+  //   const data = parseExcel(file);
+
+  //   const fields = [
+  //     { key: 'trackName', terms: ['트랙'] },
+  //     { key: 'cardinalNo', terms: ['기수'] },
+  //     { key: 'projectName', terms: ['프로젝트'] },
+  //     { key: 'round', terms: ['회차'] },
+  //     { key: 'teamNumber', terms: ['팀'] },
+  //     { key: 'realName', terms: ['이름'] },
+  //     { key: 'phoneNumberKey', terms: ['핸드폰', '휴대폰'] },
+  //     { key: 'notion', terms: ['노션'] },
+  //     { key: 'startDate', terms: ['시작'] },
+  //     { key: 'endDate', terms: ['종료'] },
+  //   ];
+
+  //   const validData = validateDataWithCoaches(data, fields);
+
+  //   const queryRunner = this.entityManager.connection.createQueryRunner();
+  //   await queryRunner.connect();
+  //   await queryRunner.startTransaction();
+
+  //   try {
+  //     const { trackName, cardinalNo, projectName, round } = validData[0];
+
+  //     const track = await this.trackRepo.findOne({
+  //       where: { trackName, cardinalNo },
+  //     });
+
+  //     if (!track)
+  //       throw new BusinessException(
+  //         `user`,
+  //         `해당 트랙(${trackName}${cardinalNo})이 존재하지 않습니다.`,
+  //         `해당 트랙(${trackName}${cardinalNo})이 존재하지 않습니다. 트랙을 먼저 생성해주세요 `,
+  //         HttpStatus.NOT_FOUND,
+  //       );
+
+  //     const existingProject = await queryRunner.manager.findOne(Project, {
+  //       where: { track: { id: track.id }, projectName, round },
+  //       relations: ['teams', 'officehours'],
+  //     });
+
+  //     if (!existingProject)
+  //       throw new BusinessException(
+  //         `project`,
+  //         `존재하지 않는 프로젝트입니다.`,
+  //         `존재하지 않는 프로젝트입니다.`,
+  //         HttpStatus.NOT_FOUND,
+  //       );
+
+  //     if (existingProject) {
+  //       await queryRunner.manager.remove(
+  //         Officehour,
+  //         existingProject.officehours,
+  //       );
+  //       await queryRunner.manager.remove(Team, existingProject.teams); // 연관된 팀 삭제
+  //       await queryRunner.manager.remove(Project, existingProject); // 프로젝트 삭제
+  //     }
+
+  //     const processedStartDate = excelDateToJSDate(validData[0].startDate);
+  //     const processedEndDate = excelDateToJSDate(validData[0].endDate);
+
+  //     const project = queryRunner.manager.create(Project, {
+  //       projectName: validData[0].projectName,
+  //       round: validData[0].round,
+  //       startDate: processedStartDate,
+  //       endDate: processedEndDate,
+  //       track,
+  //     });
+  //     await queryRunner.manager.save(project);
+
+  //     const teamCache = new Map();
+
+  //     for (const item of validData) {
+  //       const { teamNumber, phoneNumberKey, coaches, notion } = item;
+  //       const phoneNumber = phoneNumberKey.replace(/-/g, '');
+  //       let team = teamCache.get(teamNumber);
+  //       if (!team) {
+  //         team = await queryRunner.manager.findOne(Team, {
+  //           where: { teamNumber: teamNumber, project },
+  //         });
+  //         if (!team) {
+  //           team = queryRunner.manager.create(Team, {
+  //             teamNumber: teamNumber,
+  //             project,
+  //             notion,
+  //             users: [],
+  //           });
+
+  //           await queryRunner.manager.save(team);
+  //           teamCache.set(teamNumber, team);
+  //         }
+  //       }
+  //       const racer = await queryRunner.manager.findOne(User, {
+  //         where: { phoneNumber, role: UserRole.RACER },
+  //         relations: ['teams'],
+  //       });
+
+  //       if (racer && !team.users.find((u) => u.id === racer.id)) {
+  //         if (!racer.teams) {
+  //           racer.teams = [];
+  //         }
+  //         team.users.push(racer);
+  //         racer.teams.push(team);
+  //         await queryRunner.manager.save(racer);
+  //       }
+
+  //       if (coaches && coaches.length > 0) {
+  //         const coachesArr = await queryRunner.manager.find(User, {
+  //           where: {
+  //             role: UserRole.COACH,
+  //             realName: In(coaches),
+  //           },
+  //           relations: ['teams'],
+  //         });
+
+  //         for (const coach of coachesArr) {
+  //           if (!coach.teams) {
+  //             coach.teams = [];
+  //           }
+  //           if (!team.users.find((u) => u.id === coach.id)) {
+  //             team.users.push(coach);
+  //             coach.teams.push(team);
+  //             await queryRunner.manager.save(coach);
+  //           }
+  //         }
+  //       }
+  //       await queryRunner.manager.save(team);
+  //     }
+  //     await queryRunner.commitTransaction();
+  //   } catch (err) {
+  //     console.error(err);
+  //     await queryRunner.rollbackTransaction();
+  //   } finally {
+  //     await queryRunner.release();
+  //   }
+  // }
+
   async createTeamAndProject(file: Express.Multer.File) {
     const data = parseExcel(file);
-
     const fields = [
       { key: 'trackName', terms: ['트랙'] },
       { key: 'cardinalNo', terms: ['기수'] },
@@ -99,119 +237,162 @@ export class AdminService {
       { key: 'endDate', terms: ['종료'] },
     ];
 
-    const validData = validateData(data, fields);
-
-    const validCoachesData = validateCoaches(data, fields);
-
+    const validData = validateDataWithCoaches(data, fields);
     const queryRunner = this.entityManager.connection.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
 
     try {
-      const { trackName, cardinalNo, projectName, round } = validData[0];
-
-      const track = await this.trackRepo.findOne({
-        where: { trackName, cardinalNo },
-      });
-
-      if (!track)
-        throw new BusinessException(
-          `user`,
-          `해당 트랙(${trackName}${cardinalNo})이 존재하지 않습니다.`,
-          `해당 트랙(${trackName}${cardinalNo})이 존재하지 않습니다. 트랙을 먼저 생성해주세요 `,
-          HttpStatus.NOT_FOUND,
-        );
-
-      const existingProject = await queryRunner.manager.findOne(Project, {
-        where: { track: { id: track.id }, projectName, round },
-        relations: ['teams', 'officehours'],
-      });
-
-      if (existingProject) {
-        await queryRunner.manager.remove(
-          Officehour,
-          existingProject.officehours,
-        );
-        await queryRunner.manager.remove(Team, existingProject.teams); // 연관된 팀 삭제
-        await queryRunner.manager.remove(Project, existingProject); // 프로젝트 삭제
-      }
-
-      const processedStartDate = convertDate(validData[0].startDate);
-      const processedEndDate = convertDate(validData[0].endDate);
-
-      const project = queryRunner.manager.create(Project, {
-        projectName: validData[0].projectName,
-        round: validData[0].round,
-        startDate: processedStartDate,
-        endDate: processedEndDate,
-        track,
-      });
-      await queryRunner.manager.save(project);
-
-      const teamCache = new Map();
-
-      for (const item of validCoachesData) {
-        const { teamNumber, phoneNumberKey, coaches, notion } = item;
-        const phoneNumber = phoneNumberKey.replace(/-/g, '');
-        let team = teamCache.get(teamNumber);
-        if (!team) {
-          team = await queryRunner.manager.findOne(Team, {
-            where: { teamNumber: teamNumber, project },
-          });
-          if (!team) {
-            team = queryRunner.manager.create(Team, {
-              teamNumber: teamNumber,
-              project,
-              notion,
-              users: [],
-            });
-
-            await queryRunner.manager.save(team);
-            teamCache.set(teamNumber, team);
-          }
-        }
-        const racer = await queryRunner.manager.findOne(User, {
-          where: { phoneNumber, role: UserRole.RACER },
-          relations: ['teams'],
-        });
-
-        if (racer && !team.users.find((u) => u.id === racer.id)) {
-          if (!racer.teams) {
-            racer.teams = [];
-          }
-          team.users.push(racer);
-          racer.teams.push(team);
-          await queryRunner.manager.save(racer);
-        }
-
-        if (coaches && coaches.length > 0) {
-          const coachesArr = await queryRunner.manager.find(User, {
-            where: {
-              role: UserRole.COACH,
-              realName: In(coaches),
-            },
-            relations: ['teams'],
-          });
-
-          for (const coach of coachesArr) {
-            if (!coach.teams) {
-              coach.teams = [];
-            }
-            if (!team.users.find((u) => u.id === coach.id)) {
-              team.users.push(coach);
-              coach.teams.push(team);
-              await queryRunner.manager.save(coach);
-            }
-          }
-        }
-        await queryRunner.manager.save(team);
-      }
+      await this.processTeamsAndProjects(queryRunner, validData);
       await queryRunner.commitTransaction();
     } catch (err) {
       console.error(err);
       await queryRunner.rollbackTransaction();
     } finally {
       await queryRunner.release();
+    }
+  }
+
+  private async processTeamsAndProjects(queryRunner, validData) {
+    const { trackName, cardinalNo, projectName, round } = validData[0];
+    const track = await queryRunner.manager
+      .findOneOrFail(Track, {
+        where: { trackName, cardinalNo },
+      })
+      .catch(() => {
+        throw new BusinessException(
+          `user`,
+          `해당 트랙(${trackName}${cardinalNo})이 존재하지 않습니다.`,
+          `해당 트랙(${trackName}${cardinalNo})을 찾을 수 없습니다. 트랙을 먼저 생성해주세요.`,
+          HttpStatus.NOT_FOUND,
+        );
+      });
+
+    const project = await queryRunner.manager.findOne(Project, {
+      where: { track: { id: track.id }, projectName, round },
+      relations: ['teams', 'officehours'],
+    });
+
+    if (project) {
+      await this.removeProjectDependencies(queryRunner, project);
+    }
+
+    const newProject = await this.createProject(
+      queryRunner,
+      track,
+      validData[0],
+    );
+
+    await this.processTeams(queryRunner, validData, newProject);
+  }
+
+  private async removeProjectDependencies(queryRunner, project) {
+    await queryRunner.manager.remove(Officehour, project.officehours);
+    await queryRunner.manager.remove(Team, project.teams);
+    await queryRunner.manager.remove(Project, project);
+  }
+
+  private async createProject(queryRunner, track, projectData) {
+    const { projectName, round, startDate, endDate } = projectData;
+    const processedStartDate = excelDateToJSDate(startDate);
+    const processedEndDate = excelDateToJSDate(endDate);
+
+    const project = queryRunner.manager.create(Project, {
+      projectName,
+      round,
+      startDate: processedStartDate,
+      endDate: processedEndDate,
+      track,
+    });
+
+    return queryRunner.manager.save(project);
+  }
+
+  private async processTeams(queryRunner, validData, project) {
+    const teamCache = new Map();
+    const phoneNumbers = validData.map((item) =>
+      item.phoneNumberKey.replace(/-/g, ''),
+    );
+    const users: User[] = await queryRunner.manager.find(User, {
+      where: { phoneNumber: In(phoneNumbers), role: UserRole.RACER },
+      relations: ['teams'],
+    });
+
+    const userMap = new Map(users.map((user) => [user.phoneNumber, user]));
+
+    const coachNames = new Set(validData.flatMap((item) => item.coaches || []));
+    const coaches: User[] = await queryRunner.manager.find(User, {
+      where: { role: UserRole.COACH, realName: In(Array.from(coachNames)) },
+      relations: ['teams'],
+    });
+    const coachMap = new Map(coaches.map((coach) => [coach.realName, coach]));
+
+    for (const item of validData) {
+      const { teamNumber, phoneNumberKey, coaches: coachNames, notion } = item;
+      let team = teamCache.get(teamNumber);
+      if (!team) {
+        team = await this.findOrCreateTeam(
+          queryRunner,
+          teamNumber,
+          project,
+          notion,
+        );
+        teamCache.set(teamNumber, team);
+      }
+
+      const phoneNumber = phoneNumberKey.replace(/-/g, '');
+      const racer = userMap.get(phoneNumber);
+
+      if (racer && !team.users.find((user) => user.id === racer.id)) {
+        this.addRacerToTeam(racer, team);
+        await queryRunner.manager.save(racer);
+      }
+
+      if (coachNames && coachNames.length > 0) {
+        for (const name of coachNames) {
+          const coach = coachMap.get(name);
+          if (coach && !team.users.find((user) => user.id === coach.id)) {
+            this.addCoachToTeam(coach, team);
+            await queryRunner.manager.save(coach);
+          }
+        }
+      }
+    }
+  }
+
+  private async findOrCreateTeam(queryRunner, teamNumber, project, notion) {
+    let team = await queryRunner.manager.findOne(Team, {
+      where: { teamNumber, project },
+    });
+
+    if (!team) {
+      team = queryRunner.manager.create(Team, {
+        teamNumber,
+        project,
+        notion,
+        users: [],
+      });
+      await queryRunner.manager.save(team);
+    }
+
+    return team;
+  }
+
+  private addRacerToTeam(racer: User, team: Team) {
+    if (!racer.teams) {
+      racer.teams = [];
+    }
+    team.users.push(racer);
+    racer.teams.push(team);
+  }
+
+  private addCoachToTeam(coach: User, team: Team) {
+    if (!coach.teams) {
+      coach.teams = [];
+    }
+    if (!team.users.find((user) => user.id === coach.id)) {
+      team.users.push(coach);
+      coach.teams.push(team);
     }
   }
 }
